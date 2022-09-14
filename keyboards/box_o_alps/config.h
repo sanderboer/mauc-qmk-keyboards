1bf2a37e8429f2d9f6cfa5e99aa62e707db91320
Sander Boer <sanderboer@mauc.nl>
2022-09-14 21:35:02 +0200

    Too lazy to comment

---
+++ keyboards/box_o_alps/config.h
@@ -0,0 +1,50 @@
+// -*- compile-command: (concat "pushd " sndr-project-root " && lxqt-sudo make bo_o_alps:default:avrdude-split-right"); -*-
+/*
+Copyright 2022 Sander Boer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x0000
+#define DEVICE_VER 0x0011
+#define MANUFACTURER Sander Boer
+#define PRODUCT Box_o_alps
+#define DESCRIPTION A custom boxy keyboard
+
+/* key matrix size */
+//according to crkbd rows are doubled ?
+#define MATRIX_ROWS 7
+#define MATRIX_COLS 10
+
+/* key matrix pins */
+
+#define MATRIX_ROW_PINS { B3, B4, B5, B6, C15, C14, C13 } // pins A0, 15, 14, 16
+#define MATRIX_COL_PINS { A8, A9, A10, A11, A12, A4, A5, A6, A7, B0 }
+
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+// #define DEBOUNCE 5
+#define DEBOUNCE 0
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
---
+++ keyboards/box_o_alps/rules.mk
@@ -0,0 +1,42 @@
+# MCU name
+MCU = STM32F401
+BOARD = BLACKPILL_STM32_F401
+
+# Bootloader selection
+BOOTLOADER = stm32-dfu
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+
+SPLIT_KEYBOARD = no
+
+# Build Options
+#   change yes to no to disable
+#
+KEYBOARD_SHARED_EP       = yes
+# Build Options
+#   change yes to no to disable
+#
+COMBO_ENABLE = yes
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no       # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = no            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+MIDI_ENABLE = no            # MIDI support
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = no           # Audio output
+FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
